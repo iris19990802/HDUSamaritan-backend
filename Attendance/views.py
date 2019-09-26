c1d55d911a79987d348543e5766a5b6d60d59b8e
刘悦萱 <liuyuexuan@liuyuexuandeMBP.lan>
2019-09-27 05:03:25 +0800

    3rd 1、签到主功能的后端编写（sign_quest api，进行中） 2、文件目录结构调整（attendance、statics）

--- Attendance/views.py
+++ Attendance/views.py
@@ -6,8 +6,6 @@ from rest_framework import viewsets
 from rest_framework.decorators import list_route
 from rest_framework.response import Response
 
-
-
 from .models import Attendance,student_attendance
 from course.models import Course,Registration
 
@@ -16,7 +14,6 @@ import requests
 class attendanceViewSet(viewsets.ModelViewSet):
 
     queryset = Attendance.objects.all() # 如果没有这个，会报错: “assert queryset is not None”
-    
 
     # 用户按下“发送签到照片”时，调用此API
     @list_route(methods=['POST'])
@@ -26,7 +23,6 @@ class attendanceViewSet(viewsets.ModelViewSet):
         course_id = request.data['course_id']
         this_course = Course.objects.get(c_id = course_id)
 
-
         # 获取请求的用户
         this_user = request.user
         print(this_user)
@@ -34,7 +30,6 @@ class attendanceViewSet(viewsets.ModelViewSet):
             print("是教师,确实教这门课")
         else:
             return Response("Error") # 越权访问，返回error
-
 
         # 获取并存储图片
         this_image = request.FILES['file'] # UploadedFile 对象
@@ -46,7 +41,6 @@ class attendanceViewSet(viewsets.ModelViewSet):
             for chunk in this_image.chunks():
                 destination.write(chunk)
 
-
         # 处理算法部分所需参数：此班级内所有学生的：学号-名字  （生成字典格式）
         params = {}
         class_student_set = Registration.objects.filter(course=this_course) 
@@ -56,20 +50,35 @@ class attendanceViewSet(viewsets.ModelViewSet):
         print(params)
         #post_params = json.dumps(params) # 不用转成字符串
 
-
         # 调用算法部分api  
         response = requests.post('http://192.168.249.151:6000/uploader',json=params) # 指定post请求头：application/json
         # response = requests.post('http://192.168.249.151:6000/uploader',data=post_params)
         # response = requests.post('http://192.168.249.151:6000/uploader',headers={'Content-Type': 'application/json'},data=params)
         
-
-        
         # 接到返回值：“缺课学生学号”的列表
         student_abcense_lst = json.loads(response.text)
 
+        # 处理出：全班学生的到课情况json，
+        # 格式：{
+        #         'username':,
+        #         'nickname':,
+        #         'is_abcense':0/1;
+        # }
+        student_sign_list = []
+        for e in class_student_set:
+            dirc = {}
+            dirc['username'] = e.user.username
+            dirc['nickname'] = e.user.u_nickname
+            if e.user.username in student_abcense_lst:     # 如果列表中存在该元素
+                dirc['abcense'] = 0
+            else:
+                dirc['abcense'] = 1
+            student_sign_list.append(dirc)
+         
+
         # 判断拍照质量：
         # 识别人数少于班级人数1/3，则建议重拍，返回json中“quantity”属性值为“low”
-        if(len(student_abcense_lst) < len(params)/3):
+        if(len(student_abcense_lst) > len(params)*2/3):
             this_quantity = "low"
         else:
             this_quantity = "high"
@@ -80,12 +89,14 @@ class attendanceViewSet(viewsets.ModelViewSet):
         #     student_sign_list:[{
         #         'username':,
         #         'nickname':,
-        #         'is_abcense':0/1;
+        #         'abcense':0/1;  (1表示到了，0表示未到)
         #     }]
         # }
-        return Response("1")
-    
 
+        return Response({
+            "quantity":this_quantity,
+            "student_sign_list":student_sign_list
+        })
 
 
     # 用户手动修改，并“确认到课情况”后，才会真正把数据写入 abcense 数据库
